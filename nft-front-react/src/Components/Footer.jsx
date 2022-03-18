40c1b9dc1c46c92457045a6b8f88a8d593413f2b
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2022-03-18 17:41:37 +0100

    Componetizing front end directory

---
+++ nft-front-react/src/Components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import twitterLogo from '../assets/twitter-logo.svg';
+import "../styles/App.css";
+// Constants
+const TWITTER_HANDLE = 'weedarg';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const Footer = () => {
+
+    return (
+        <div className="container">
+        <div className="footer-container">
+        <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+        <a
+        className="footer-text"
+        href={TWITTER_LINK}
+        target="_blank"
+        rel="noreferrer"
+        >{`built by @${TWITTER_HANDLE}`}</a>
+        </div>
+        </div>
+
+
+    );
+    }
+export default Footer;
+
