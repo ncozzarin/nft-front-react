1085672ec2e1725554ea6566809a71b95423bcbc
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2022-03-21 10:47:39 +0100

    Separing title and connection landing part

--- nft-front-react/src/App.js
+++ nft-front-react/src/App.js
@@ -5,6 +5,7 @@ import "./styles/App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Components/Home';
 import About from './Components/About';
+import Connect from './Components/Connect';
 
 
 
@@ -16,7 +17,8 @@ const App = () => {
       <TopSection ></TopSection>
       <Footer></Footer>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Connect />} />
+        <Route path="/home" element={<Home />} />
         <Route path="about" element={<About />} />
       </Routes>
     </div>
