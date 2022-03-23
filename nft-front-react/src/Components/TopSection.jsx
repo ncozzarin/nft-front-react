1b9be54ed5e095b71ec7b75a355aaa6351bfed7e
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2022-03-23 10:30:41 +0100

    added section buttons and refactor title

--- nft-front-react/src/Components/TopSection.jsx
+++ nft-front-react/src/Components/TopSection.jsx
@@ -1,5 +1,6 @@
 
 import React from "react";
+import "../styles/output.css";
 
 
  export default function TopSection() {
@@ -8,7 +9,7 @@ import React from "react";
    return (
     <div className="container-base">
       <div className="header-container">
-        <p className="header gradient-text">Weedmaniac</p>    
+        <p className='text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-cyan-400 to-blue-400 font-sans '>Weedmaniac</p>    
       </div>
     </div>
 
